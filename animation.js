c40a712df0343ac25e78ba552ea3b06a421979a9
showa11 <showa11@hotmail.com>
2017-07-28 22:35:19 +0800

    inital

---
+++ animation.js
@@ -0,0 +1,8 @@
+/*
+* @Author: showa11
+* @Date:   2017-07-28 22:27:40
+* @Last Modified by:   showa11
+* @Last Modified time: 2017-07-28 22:27:44
+*/
+
+'use strict';
---
+++ main.js
@@ -0,0 +1,8 @@
+/*
+* @Author: showa11
+* @Date:   2017-07-28 22:26:52
+* @Last Modified by:   showa11
+* @Last Modified time: 2017-07-28 22:26:58
+*/
+
+'use strict';
